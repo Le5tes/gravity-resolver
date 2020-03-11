0ad751f6c057ec4d808a0a880a75d667c224ab94
Tim Williamson <timothy.williamson@santander.co.uk>
2020-03-11 20:36:14 +0000

    feat(index.js) export classes

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const resolver = require('./resolver/resolver').Resolver
+const barnesHutTreeResolver = require('.barnes-hut-tree-resolver/barnes-hut-tree-resolver').BarnesHutTreeResolver
+
+module.exports = { 
+    GravityResolver: resolver, 
+    BarnesHutTreeResolver: barnesHutTreeResolver
+}
